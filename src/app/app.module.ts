72f88be43cf08a507d95e2ded642d06c3bc5da7f
Boopathyraj Thangadurai <boopathyraj.t@KGFSL.COM>
2017-11-01 10:38:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { TemplateDrivenFormComponent }  from './template-driven-form.component';
+import { ReactiveFormComponent }  from './reactive-form.component';
+import { UserService } from './services/user-service';
 
 @NgModule({
+  imports: [     
+        BrowserModule,
+	FormsModule, 
+	ReactiveFormsModule
+  ],
   declarations: [
-    AppComponent
+        AppComponent,
+	TemplateDrivenFormComponent,
+	ReactiveFormComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+        UserService
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+        AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule { } 
